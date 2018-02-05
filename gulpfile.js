f9cf38ba9930bea6be6a6899c949bd10f5f609cf
JBerg60 <johnnyvandeberg@gmail.com>
2018-02-05 23:19:47 +0100

    readme update

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,20 @@
 /*
  *	Simple gulp file for automated workflow
 */
-var gulp       = require('gulp');
-var livereload = require('gulp-livereload');
-var watch      = require('gulp-watch');
-var connect    = require('gulp-connect');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
 
-var paths = {
-	sass:       ['./scss/**/*.scss'],
-	templates:  ['./**/*.html']
-  };
-  
-  gulp.task('css', function () {
-	gulp.watch(paths.sass, ['sass']);
-  });
+gulp.task('css', function () {
+	//gulp.watch(paths.sass, ['sass']);
+});
 
-  gulp.task('watch', function () {
-	gulp.watch(paths.sass, ['sass']);
-  });
-  
-  gulp.task('serve', function() {
-	connect.server({
-	  livereload: true,
-	  port: 1337,
-	  host: "localhost",
-	  root: 'dest'
-	});
-  });
-
-  gulp.task('serve', ['css'], function() {
+gulp.task('serve', ['css'], function() {
     
     browserSync.init({
-        server: "./"
+        server: "./dist/"
     });
 
-    gulp.watch("./sass/main.scss", ['sass']);
+    //gulp.watch("./sass/main.scss", ['sass']);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
